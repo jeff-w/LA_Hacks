4e039e4c911b8e591bffdbb907aae6378fe05a3a
U-Fatty\Jeffrey <Jeffrey@Fatty.(none)>
2014-04-12 16:11:03 -0700

    4:11pm

--- jav.js
+++ jav.js
@@ -1,7 +1,8 @@
-/*$(document).ready(function(){
-    
-    
-});*/
+/*global $ */                   //declare global vars in a comment..
+/*jslint white: true */
+
+var latitude;
+var longitude;
 
 /*LOTS of complaints about indentation and spacing wtf...*/
 
@@ -10,9 +11,11 @@ function displayLocation(position) {
     "use strict";
     //build text string including co-ordinate data passed in parameter
     var displayText = "User latitude is " + position.coords.latitude + " and longitude is " + position.coords.longitude;
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
 
     //display the string for demonstration
-    document.getElementById("locationData").innerHTML = displayText;
+    document.getElementById("locationData").innerHTML = "lat is " + latitude + ", long is " + longitude;
 }
 
 
@@ -45,6 +48,8 @@ function displayError(error) {
 function getUserLocation() {
     "use strict";   //dumb complaint
 
+    window.console.log("enter getUserLocation() function");
+    
     //check if the geolocation object is supported, if so get position
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(displayLocation, displayError);
@@ -52,3 +57,10 @@ function getUserLocation() {
         document.getElementById("locationData").innerHTML = "Sorry - your browser doesn't support geolocation!";
     }
 }
+
+
+
+$(document).ready(function(){
+    "use strict";
+    
+});
